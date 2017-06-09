1ecf37505699906a7539dcbcc1301f5e048b335a
Felipez-Maturana <felipe.maturana.g@usach.cl>
2017-06-09 03:04:30 -0400

    jquery

--- app/controllers/pags_controller.rb
+++ app/controllers/pags_controller.rb
@@ -1,7 +1,20 @@
 class PagsController < ApplicationController
+	# Este metodo se llama antes de cualquier accion
+	# CALLBACK
+	before_action :get_data
   def pag1
+  	 render layout: "landing"
   end
 
   def pag2
   end
+  
+  def pag3
+  	
+  end
+
+  def get_data
+  	@users=["Alumno1","Alumno2","Alumno3","Alumno4"]
+  end
+
 end
